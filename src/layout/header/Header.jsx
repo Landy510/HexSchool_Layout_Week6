0cd9e529da759696bd9801f45143ffc8720a4094
Landy510 <randy.chen@tpisoftware.com>
2023-09-03 16:15:47 +0800

    fix: logo image imported path

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,4 +1,4 @@
-import logoImg from '@/assets/images/logo.svg';
+import logoImg from '@/assets/images/Logo.svg';
 import { memo, useState } from 'react';
 import { Link } from 'react-router-dom';
 const Header = memo(function Header () {
